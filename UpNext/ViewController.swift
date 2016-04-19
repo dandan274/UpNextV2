90a4c1c8ec5dc0eb85c7a3ca12e1af5ddaff4710
DANIELLE PINTZ <DANDAN274@GMAIL.COM>
2016-04-19 12:02:43 -0400

    Initial Commit

---
+++ UpNext/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UpNext
+//
+//  Created by Danielle Pintz on 4/19/16.
+//  Copyright © 2016 Danielle Pintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
